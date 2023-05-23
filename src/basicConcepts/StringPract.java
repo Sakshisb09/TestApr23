2180a2a47d79474e9ae870cdf24414fde478032b
Sakshi Bhagwatkar <sakshibhagwatkar09@gmail.com>
2023-05-23 08:57:06 +0530

    Branch B2 scenario 2

--- src/basicConcepts/StringPract.java
+++ src/basicConcepts/StringPract.java
@@ -2,7 +2,7 @@ package basicConcepts;
 
 public class StringPract {
 	public static void main(String[] args) {
-		System.out.println("Hello class B1..........");
+		System.out.println("Hello branch B2");
 		System.out.println("Resolved conflict");
 	}
 
